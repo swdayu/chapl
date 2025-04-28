9efa458928f6027f39c504424f0499eea0a7b9ad
swdayu <12118648+swdayu@users.noreply.github.com>
2025-04-28 23:20:21 +0800

    Update conc test code

--- config/conc_test/main.c
+++ config/conc_test/main.c
@@ -1,22 +1,23 @@
-#include "conc/conc.h"
+#define PRH_IMPLEMENTATION
+#define PRH_CONC_INCLUDE
+#define PRH_CONC_STRIP_PREFIX
+#define PRH_CONC_IMPLEMENTATION
+#define PRH_CONC_TEST
+#include "prh_include.h"
 
-int threadproc(struct thrd *t)
+int threadproc(thrd_t *thrd)
 {
-    threads_current_thrd_info(t);
-    printf("[thread %d] index %d\n", threads_get_thrid(t), t->index);
     return 0;
 }
 
 int main(int argc, char **argv)
 {
-    direct_test();
+    thrd_struct *s = thread_init(30, 3, 0);
+    thread_create(s, threadproc, 1, 0);
+    thread_create(s, threadproc, 0, 0);
+    thread_create(s, threadproc, 0, 0);
+    thread_join(&s, NULL);
 
-    struct thrd *t = threads_init(30, 3, NULL);
-    printf("\n\n\n[GNU][threads][main %d]\n", threads_get_thrid(t));
-    threads_create(t, threadproc, 1);
-    threads_create(t, threadproc, 0);
-    threads_create(t, threadproc, 0);
-    threads_join(t);
-
+    prh_conc_test();
     return 0;
 }
