5be74af9f8a6ef5280967858adebb0ae28b3fe1f
swdayu <12118648+swdayu@users.noreply.github.com>
2025-05-12 23:45:22 +0800

    Update for code clean up

--- config/conc_test/main.c
+++ config/conc_test/main.c
@@ -1,8 +1,7 @@
-#define PRH_IMPLEMENTATION
 #define PRH_CONC_INCLUDE
 #define PRH_CONC_STRIP_PREFIX
 #define PRH_CONC_IMPLEMENTATION
-#define PRH_CONC_TEST
+#define PRH_TEST_IMPLEMENTATION
 #include "prh_include.h"
 
 int threadproc(thrd_t *thrd)
@@ -12,12 +11,12 @@ int threadproc(thrd_t *thrd)
 
 int main(int argc, char **argv)
 {
-    thrd_struct *s = thread_init(30, 3, 0);
+    thrdpool_t *s = thread_init(30, 3, 0);
     thread_create(s, threadproc, 1, 0);
     thread_create(s, threadproc, 0, 0);
     thread_create(s, threadproc, 0, 0);
     thread_join(&s, NULL);
 
-    prh_conc_test();
+    prh_test_code();
     return 0;
 }
